82209758e30a3ce8ac0aa39c423b34fc408fd69d
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-02-02 18:03:57 +0100

    pun.c

---
+++ pun.c
@@ -0,0 +1,11 @@
+/* Name: pun.c                  */
+/* Purpose: Prints a bad C pun  */
+/* Author: Upal Bhattacharya    */
+
+#include <stdio.h>
+
+int maid(void)
+{
+  printf("To C, or not to C, that is the question.\n");
+  return 0;
+}
